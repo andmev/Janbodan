fc105f5470c73efc48a7d6a9968dfee59d549dd6
Andrii Medvediev <a.medvedev@me.com>
2025-02-25 11:34:09 +0100

    Initial Commit

---
+++ Janbodan/JanbodanApp.swift
@@ -0,0 +1,32 @@
+//
+//  JanbodanApp.swift
+//  Janbodan
+//
+//  Created by Andrii Medvediev on 25.02.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct JanbodanApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ JanbodanTests/JanbodanTests.swift
@@ -0,0 +1,17 @@
+//
+//  JanbodanTests.swift
+//  JanbodanTests
+//
+//  Created by Andrii Medvediev on 25.02.2025.
+//
+
+import Testing
+@testable import Janbodan
+
+struct JanbodanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
